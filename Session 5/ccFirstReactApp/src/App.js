38c26d88e104510efc2e8759317137013fbee7ba
Suraj Singh <90023945+surajskit@users.noreply.github.com>
2021-09-24 00:10:11 +0530

    Add files via upload

---
+++ Session 5/ccFirstReactApp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+   <h1>
+     Hello  Cognitive Clouds Interns!
+   </h1>
+    </div>
+  );
+}
+
+export default App;
